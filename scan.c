c7f043694eb92342a11dc8958ad6bc84b7ab066f
Ernesto Martinez <ernesto.martinez.07.12.05@gmail.com>
2024-01-24 11:21:58 -0500

    scan fixed

--- scan.c
+++ scan.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
@@ -16,7 +15,20 @@
 #define O_BINARY 0
 #endif
 
-int main(int argc, char **argv) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <clamav.h>
+
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif
+
     /*
     * No seas cochino, piensa, no incluyas librerias que no vas a utilizar
     * No hagas las cosas por bonito, que termina quedando puerco tu programa.
@@ -25,6 +37,7 @@ int main(int argc, char **argv) {
     * No pienses que vas a hackear la NASA de ahora pa' ahorita, haz las cosas con calma.
     */
 
+void scan_dir(char *dir_name, int dipth) {
     int fd, ret;
     unsigned long int size = 0;
     unsigned int sigs      = 0;
@@ -34,21 +47,18 @@ int main(int argc, char **argv) {
     struct cl_engine *engine;
     struct cl_scan_options options;
 
-    if (argc != 2) {
-        printf("Usage: %s file\n", argv[0]);
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+
+    if((dp = opendir(dir_name)) == -1) {
+        printf("Error, Can't open dirctory: %s", dir_name);
         return 2;
     }
-
-    filename = argv[1];
-
-    if ((fd = open(argv[1], O_RDONLY | O_BINARY)) == -1) {
-        printf("Can't open file %s\n", argv[1]);
-        return 2;
-    }
-
+    
     if((ret = cl_init(CL_INIT_DEFAULT)) != CL_SUCCESS) {
-        printf("cl_init() error: %s", cl_strerror(ret));
-        return 2;
+       printf("cl_init() error: %s", cl_strerror(ret));
+       return 2;
     }
     if(!(engine = cl_engine_new())) {
         printf("Error, can't create new engine");
@@ -75,6 +85,27 @@ int main(int argc, char **argv) {
     options.parse |= ~0;                           /* enable all parsers */
     options.general |= CL_SCAN_GENERAL_HEURISTICS; /* enable heuristic alert options */
 
+    while ((entry = readdir(dp)) != NULL) {
+    lstat(entry->d_name, &statbuf);
+    if (S_ISDIR(statbuf.st_mode)) {
+        if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+            continue;
+
+        printf("%*s[+] %s/\n", depth*2, "", entry->d_name);
+        scan_dir(entry->d_name, depth + 1);
+    } else {
+    if (argc != 2) {
+        printf("Usage: %s file\n", entry->d_name);
+        return 2;
+    }
+
+    filename = entry->d_name;
+
+    if ((fd = open(filename, O_RDONLY | O_BINARY)) == -1) {
+        printf("Can't open file %s\n", argv[1]);
+        return 2;
+    }
+
     if ((ret = cl_scandesc(fd, filename, &virname, &size, engine, &options)) == CL_VIRUS) {
         printf("Virus detected: %s\n", virname);
     } else {
@@ -89,10 +120,23 @@ int main(int argc, char **argv) {
     }
     close(fd);
 
+        
+        printf("%*s- %s\n", depth*2, "", entry->d_name);
+    }
+}
+
+chdir("..");
+closedir(dp);
+
     cl_engine_free(engine);
 
     mb = size * (CL_COUNT_PRECISION / 1024) / 1024.0;
     printf("Data scanned: %2.2Lf MB\n", mb);
 
     return ret == CL_VIRUS ? 1 : 0;
+}
+
+int main() {
+    scan_dir(".");
+    return 0;
 }
