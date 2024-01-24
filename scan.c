4af4f19d3cbccbe452cd360b28bfea0d9586d65d
Ernesto Martinez <ernesto.martinez.07.12.05@gmail.com>
2024-01-24 11:38:27 -0500

    scan fixed

--- scan.c
+++ scan.c
@@ -37,7 +37,8 @@
     * No pienses que vas a hackear la NASA de ahora pa' ahorita, haz las cosas con calma.
     */
 
-void scan_dir(char *dir_name, int dipth) {
+void scan_dir(char *dir_name, int depth) {
+
     int fd, ret;
     unsigned long int size = 0;
     unsigned int sigs      = 0;
@@ -51,11 +52,13 @@ void scan_dir(char *dir_name, int dipth) {
     struct dirent *entry;
     struct stat statbuf;
 
+/*Open directory*/
     if((dp = opendir(dir_name)) == -1) {
         printf("Error, Can't open dirctory: %s", dir_name);
         return 2;
     }
-    
+/**/
+/*Create engine*/    
     if((ret = cl_init(CL_INIT_DEFAULT)) != CL_SUCCESS) {
        printf("cl_init() error: %s", cl_strerror(ret));
        return 2;
@@ -64,7 +67,8 @@ void scan_dir(char *dir_name, int dipth) {
         printf("Error, can't create new engine");
         return 2;
     }
-
+/**/
+/*Load Virus Database*/
     if((ret = cl_load(cl_retdbdir(), engine, &sigs, CL_DB_STDOPT)) != CL_SUCCESS) {
         printf("cl_load() error: %s", cl_strerror(ret));
         close(fd);
@@ -73,7 +77,8 @@ void scan_dir(char *dir_name, int dipth) {
     }
 
     printf("Loading %u", sigs);
-
+/**/
+/*Compile engine*/
     if((ret = cl_engine_compile(engine)) != CL_SUCCESS) {
         printf("cl_engine_compile error: %s", cl_strerror(ret));
         close(fd);
@@ -84,7 +89,8 @@ void scan_dir(char *dir_name, int dipth) {
     memset(&options, 0, sizeof(struct cl_scan_options));
     options.parse |= ~0;                           /* enable all parsers */
     options.general |= CL_SCAN_GENERAL_HEURISTICS; /* enable heuristic alert options */
-
+/**/
+/*Scan*/
     while ((entry = readdir(dp)) != NULL) {
     lstat(entry->d_name, &statbuf);
     if (S_ISDIR(statbuf.st_mode)) {
@@ -94,10 +100,6 @@ void scan_dir(char *dir_name, int dipth) {
         printf("%*s[+] %s/\n", depth*2, "", entry->d_name);
         scan_dir(entry->d_name, depth + 1);
     } else {
-    if (argc != 2) {
-        printf("Usage: %s file\n", entry->d_name);
-        return 2;
-    }
 
     filename = entry->d_name;
 
@@ -110,7 +112,7 @@ void scan_dir(char *dir_name, int dipth) {
         printf("Virus detected: %s\n", virname);
     } else {
         if (ret == CL_CLEAN) {
-            printf("No virus detected.\n");
+            printf("%s OK", filename);
         } else {
             printf("Error: %s\n", cl_strerror(ret));
             cl_engine_free(engine);
