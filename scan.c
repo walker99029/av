5c72612d1b48cf5a9d03a43c8c82b7bbf0aae917
Ernesto Martinez <ernesto.martinez.07.12.05@gmail.com>
2024-01-24 11:46:23 -0500

    scan fixed

--- scan.c
+++ scan.c
@@ -37,7 +37,7 @@
     * No pienses que vas a hackear la NASA de ahora pa' ahorita, haz las cosas con calma.
     */
 
-void scan_dir(char *dir_name, int depth) {
+int scan_dir(char *dir_name, int depth) {
 
     int fd, ret;
     unsigned long int size = 0;
@@ -136,6 +136,83 @@ closedir(dp);
     printf("Data scanned: %2.2Lf MB\n", mb);
 
     return ret == CL_VIRUS ? 1 : 0;
+}
+
+int scan_file(int argc, char **argv) {
+
+    int fd, ret;
+    unsigned long int size = 0;
+    unsigned int sigs      = 0;
+    long double mb;
+    const char *virname;
+    const char *filename;
+    struct cl_engine *engine;
+    struct cl_scan_options options;
+
+    if (argc != 2) {
+        printf("Usage: %s file\n", argv[0]);
+        return 2;
+    }
+
+    filename = argv[1];
+
+    if ((fd = open(argv[1], O_RDONLY | O_BINARY)) == -1) {
+        printf("Can't open file %s\n", argv[1]);
+        return 2;
+    }
+
+    if ((ret = cl_init(CL_INIT_DEFAULT)) != CL_SUCCESS) {
+        printf("Can't initialize libclamav: %s\n", cl_strerror(ret));
+        return 2;
+    }
+
+    if (!(engine = cl_engine_new())) {
+        printf("Can't create new engine\n");
+        return 2;
+    }
+
+    if ((ret = cl_load(cl_retdbdir(), engine, &sigs, CL_DB_STDOPT)) != CL_SUCCESS) {
+        printf("cl_load: %s\n", cl_strerror(ret));
+        close(fd);
+        cl_engine_free(engine);
+        return 2;
+    }
+
+    printf("Loaded %u signatures.\n", sigs);
+
+       if ((ret = cl_engine_compile(engine)) != CL_SUCCESS) {
+        printf("Database initialization error: %s\n", cl_strerror(ret));
+        cl_engine_free(engine);
+        close(fd);
+        return 2;
+    }
+
+    memset(&options, 0, sizeof(struct cl_scan_options));
+    options.parse |= ~0;                           /* enable all parsers */
+    options.general |= CL_SCAN_GENERAL_HEURISTICS; /* enable heuristic alert options */
+
+    if ((ret = cl_scandesc(fd, filename, &virname, &size, engine, &options)) == CL_VIRUS) {
+        printf("Virus detected: %s\n", virname);
+    } else {
+        if (ret == CL_CLEAN) {
+            printf("No virus detected.\n");
+        } else {
+            printf("Error: %s\n", cl_strerror(ret));
+            cl_engine_free(engine);
+            close(fd);
+            return 2;
+        }
+    }
+    close(fd);
+
+    cl_engine_free(engine);
+
+    mb = size * (CL_COUNT_PRECISION / 1024) / 1024.0;
+    printf("Data scanned: %2.2Lf MB\n", mb);
+
+    return ret == CL_VIRUS ? 1 : 0;
+
+
 }
 
 int main() {
